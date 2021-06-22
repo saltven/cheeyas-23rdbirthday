b6eb5c58d254ba5eae9f4960584f33574e0291b5
saltven <39688612+saltven@users.noreply.github.com>
2021-06-22 20:21:53 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    overflow-x: hidden;
+    margin: 0;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <body class="parent-wrap">
+    <div class="header-wrap">
+      <vue-particles class="particle"
+        color="#fff"
+        :particleOpacity="0.7"
+        :particlesNumber="90"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      <div class="nav-bar">
+
+      </div>
+      </vue-particles>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+
+<style>
+body {
+  margin: 0;
+}
+.header-wrap {
+  background: #000;
+}
+.particle {
+  height: 100vh;
+}
+
+
+</style>
