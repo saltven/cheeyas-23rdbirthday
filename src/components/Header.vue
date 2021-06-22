5d11565c025a81e772075890e3071de427ef655b
salt <ammarhazrinn@gmail.com>
2021-06-23 03:50:06 +0800

    C3: windows pc 1st

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -34,6 +34,9 @@
           </div>
         </div>
       </div>
+      <div class="test">
+        test1212
+      </div>
     </div>
   </body>
 </template>
@@ -49,8 +52,9 @@ export default {
 </script>
 
 
-<style lang="scss">
+<style>
 body {
   margin: 0;
 }
+
 </style>
