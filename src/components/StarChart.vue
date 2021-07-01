8cf8f17d1465c27ecb9d22c82ba6fe1e4e1261e8
Mohammad Ammar Bin Hazrin Chong <m.ammar@hitachi-ebworx.com>
2021-07-01 18:02:39 +0800

    fixed star map section

--- src/components/StarChart.vue
+++ src/components/StarChart.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="section-container-starchart">
-        
+        <div class="starchart-items">
+            <div class="starchart-title">
+                <h1>Your Star Chart</h1>
+            </div>
+            <div class="starchart-signs">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
