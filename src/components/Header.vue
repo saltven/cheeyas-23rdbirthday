c84c2500bdbbb914ad37d4365644e2ba47f08c93
salt <ammarhazrinn@gmail.com>
2021-07-08 03:44:37 +0800

    removed navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,7 +20,22 @@
       >
       </vue-particles>
       <div class="nav-bar">
-          <Navbar/>
+          <div class="nav-items">
+          <b-navbar toggleable="lg" type="dark" >
+              <b-navbar-brand href="#" class="logo">SZ</b-navbar-brand>
+
+              <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+              <b-collapse id="nav-collapse" is-nav>
+                  <b-navbar-nav> 
+                      <b-nav-item href="#">STAR MAP</b-nav-item>
+                      <b-nav-item href="#">STAR CHART</b-nav-item>
+                      <b-nav-item href="#">WISHES</b-nav-item>
+                      <b-nav-item href="#">SUBMIT A WISH</b-nav-item>
+                  </b-navbar-nav>
+              </b-collapse> -->
+          </b-navbar>
+        </div>
       </div>
       <div class="title-text">
         <div class="title">
@@ -40,7 +55,6 @@
 <script>
 import '../assets/_main.scss'
 import { setTimeout } from 'timers';
-import Navbar from './Navbar'
   export default {
     name: 'Header',
     data: () => {
@@ -56,7 +70,6 @@ import Navbar from './Navbar'
       }
     },
     components: {
-      Navbar
     },
     methods: {
        typeText() {
