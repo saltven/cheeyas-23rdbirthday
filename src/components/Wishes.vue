3efb6a70b8deacf078253c81d74282faed1e846d
salt <ammarhazrinn@gmail.com>
2021-07-22 02:33:00 +0800

    most things are ok

--- src/components/Wishes.vue
+++ src/components/Wishes.vue
@@ -1,12 +1,36 @@
 <template >
     <div class="section-container-wishes">
-        
+        <div class="wishes-items">
+            <div class="wishes-title">
+                <h1>Birthday Wishes</h1>
+            </div>
+            <div class="wishes-container">
+                <div class="wishes-outer">
+                    <div class="wishes-inner" v-for="wish in wishes" :key="wish.id">
+                        <div class="avatar-img">
+                            <img :src="require(`@/assets/images/${wish.image}`)" alt="nada">
+                        </div>
+                        <div class="wish-container">
+                            <h4>{{wish.name}}</h4>
+                            <p>{{wish.wish}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import '../assets/_main.scss'
+import wishData from './wishes.json'
+
 export default {
     name: 'Wishes',
+    data() {
+        return {
+            wishes: wishData
+        }
+    },
 }
 </script>
 <style lang="">
